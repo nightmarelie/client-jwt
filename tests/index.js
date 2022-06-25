291c85ffc290b9be6d18c5f8f84484a7e06ea1dd
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-06-24 21:15:34 -0600

    test: add more than once case

--- tests/index.js
+++ tests/index.js
@@ -56,3 +56,96 @@ test("Logout removes token information", async t => {
   t.falsy(mock.history.get[0].headers.Authorization);
 });
 
+test("Correctly retries request when got 401 with new token", async t => {
+  const { mock, api } = t.context;
+  const LOGIN_REQUEST = {
+    login: "foo",
+    password: "foo",
+  };
+  const LOGIN_RESPONSE = {
+    token: "TOKEN",
+    refreshToken: "REFRESH_TOKEN",
+  };
+
+  const REFRESH_REQUEST = {
+    refreshToken: LOGIN_RESPONSE.refreshToken,
+  };
+  const REFRESH_RESPONSE = {
+    token: "TOKEN2",
+    refreshToken: "REFRESH_TOKEN2",
+  };
+
+  mock.onPost("/auth/login", LOGIN_REQUEST).reply(200, LOGIN_RESPONSE);
+  mock
+    .onPost("/auth/refresh", REFRESH_REQUEST)
+    .replyOnce(200, REFRESH_RESPONSE);
+  mock.onGet("/users").reply(config => {
+    const { Authorization: auth } = config.headers;
+    if (auth === `Bearer ${LOGIN_RESPONSE.token}`) {
+      return [401];
+    }
+    if (auth === `Bearer ${REFRESH_RESPONSE.token}`) {
+      return [200, []];
+    }
+    return [404];
+  });
+
+  await api.login(LOGIN_REQUEST);
+  await api.getUsers();
+  t.is(mock.history.get.length, 2);
+  t.is(
+    mock.history.get[1].headers.Authorization,
+    `Bearer ${REFRESH_RESPONSE.token}`
+  );
+});
+
+test("Correctly fails request when got non-401 error", async t => {
+  const { mock, api } = t.context;
+  mock.onGet("/users").reply(404);
+  await t.throws(async () => {
+    await api.getUsers();
+  });
+});
+
+test("Does not consumes token more than once", async t => {
+  const { mock, api } = t.context;
+  const LOGIN_REQUEST = {
+    login: "foo",
+    password: "foo",
+  };
+  const LOGIN_RESPONSE = {
+    token: "TOKEN",
+    refreshToken: "REFRESH_TOKEN",
+  };
+
+  const REFRESH_REQUEST = {
+    refreshToken: LOGIN_RESPONSE.refreshToken,
+  };
+  const REFRESH_RESPONSE = {
+    token: "TOKEN2",
+    refreshToken: "REFRESH_TOKEN2",
+  };
+
+  mock.onPost("/auth/login", LOGIN_REQUEST).reply(200, LOGIN_RESPONSE);
+  mock
+    .onPost("/auth/refresh", REFRESH_REQUEST)
+    .replyOnce(200, REFRESH_RESPONSE);
+
+  mock.onGet("/users").reply(config => {
+    const { Authorization: auth } = config.headers;
+    if (auth === `Bearer ${LOGIN_RESPONSE.token}`) {
+      return [401];
+    }
+    if (auth === `Bearer ${REFRESH_RESPONSE.token}`) {
+      return [200, []];
+    }
+    return [404];
+  });
+
+  await api.login(LOGIN_REQUEST);
+  await Promise.all([api.getUsers(), api.getUsers()]);
+  t.is(
+    mock.history.post.filter(({ url }) => url === "/auth/refresh").length,
+    1
+  );
+});
