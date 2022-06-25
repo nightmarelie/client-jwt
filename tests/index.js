131c42b0c7bc7a3f54c956ac9777289b5a2b99f4
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-06-24 21:14:50 -0600

    test: add logout case

--- tests/index.js
+++ tests/index.js
@@ -34,3 +34,25 @@ test("Login captures token information", async t => {
   );
 });
 
+test("Logout removes token information", async t => {
+  const { mock, api } = t.context;
+  const LOGIN_REQUEST = {
+    login: "foo",
+    password: "foo",
+  };
+  const LOGIN_RESPONSE = {
+    token: "TOKEN",
+    refreshToken: "REFRESH_TOKEN",
+  };
+
+  mock.onPost("/auth/login", LOGIN_REQUEST).reply(200, LOGIN_RESPONSE);
+  mock.onGet("/users").reply(200, []);
+
+  await api.login(LOGIN_REQUEST);
+  await api.logout();
+  await api.getUsers();
+
+  t.is(mock.history.get.length, 1);
+  t.falsy(mock.history.get[0].headers.Authorization);
+});
+
