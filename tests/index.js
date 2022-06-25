2b9cb37ccff5ef2e2006292a90debd82f6019ad5
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-06-24 21:14:00 -0600

    test: add login test

---
+++ tests/index.js
@@ -0,0 +1,36 @@
+import { test } from "ava";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+
+import Api from "../src";
+
+test.beforeEach(t => {
+  const client = axios.create();
+  t.context.mock = new MockAdapter(client);
+  t.context.api = new Api({ client });
+});
+
+test("Login captures token information", async t => {
+  const { mock, api } = t.context;
+  const LOGIN_REQUEST = {
+    login: "foo",
+    password: "foo",
+  };
+  const LOGIN_RESPONSE = {
+    token: "TOKEN",
+    refreshToken: "REFRESH_TOKEN",
+  };
+
+  mock.onPost("/auth/login", LOGIN_REQUEST).reply(200, LOGIN_RESPONSE);
+  mock.onGet("/users").reply(200, []);
+
+  await api.login(LOGIN_REQUEST);
+  await api.getUsers();
+
+  t.is(mock.history.get.length, 1);
+  t.is(
+    mock.history.get[0].headers.Authorization,
+    `Bearer ${LOGIN_RESPONSE.token}`
+  );
+});
+
